123e9a3a1b678e23025aabbb15783e8cd9da6a49
yeraldycarpio <2430933@clases.edu.sv>
2023-08-15 15:09:25 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding/Alumno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DataBinding
+{
+    //hacemos la clase alumno, y con INotifyPropertyChanged se notificara el cambio que se hizo
+    public class Alumno:INotifyPropertyChanged
+    {
+        //Cada vez que se haga un cambio, se notificara 
+        public event PropertyChangedEventHandler PropertyChanged;
+        //esto almacenara el nombre
+        string nombre = string.Empty;
+
+
+        public string Nombre
+        {
+            //devuelve el valor actual almacenado
+            get => nombre;
+            //cuando se quiera asignar un nuevo valor a la propiedad nombre se mostrara dentro de este bloque
+            set
+            {
+                //esto verificara si el nuevo valor es igual al valor actual de nombre
+                if (nombre == value)
+                    return;
+                //si el valor es diferente se actualizara al nuevo valor
+                nombre = value;
+                //Esto notificara el cambio a la propiedad nombre
+                onPropertyChanged(nameof(Nombre));
+            //tambien se le notifica a la propiedad nombre
+                onPropertyChanged(nameof(MostrarNombre));
+            }
+        }
+        //esto mostrara el nombre ingresado
+        public string MostrarNombre=>$"Nombre ingresado:{Nombre}";
+        //llama la propiedad PropertyChanged, para que notifique el cambio
+        void onPropertyChanged(string nombre)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+    }
+}
---
+++ DataBinding/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBinding;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
